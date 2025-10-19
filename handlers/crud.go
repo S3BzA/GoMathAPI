10d27a2c73d8f353f78ddc5441243db7d8eb3897
Sebastian Guimaraens <sebguim2002@gmail.com>
2025-10-19 21:06:28 +0200

    Add CRUD handler functions for API

---
+++ handlers/crud.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Read(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
